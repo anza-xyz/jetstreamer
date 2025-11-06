c9a778e77a0fbdb5145f6e02215b566ca253cee5
Sam Johnson <sam@durosoft.com>
2025-11-06 18:09:13 -0500

    run instruction tracking

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use jetstreamer::JetstreamerRunner;
-use jetstreamer_plugin::plugins::program_tracking::ProgramTrackingPlugin;
+use jetstreamer_plugin::plugins::instruction_tracking::InstructionTrackingPlugin;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     JetstreamerRunner::default()
         .with_log_level("info")
         .parse_cli_args()?
-        .with_plugin(Box::new(ProgramTrackingPlugin))
+        .with_plugin(Box::new(InstructionTrackingPlugin))
         .run()
         .map_err(|err| -> Box<dyn std::error::Error> { Box::new(err) })?;
     Ok(())

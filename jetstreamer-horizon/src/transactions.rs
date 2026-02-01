e2250a16027ab62b19f61b8f27433ab8d375693c
Sam Johnson <sam@durosoft.com>
2026-02-01 01:26:30 -0500

    remove redundancies

--- jetstreamer-horizon/src/transactions.rs
+++ jetstreamer-horizon/src/transactions.rs
@@ -1,25 +1,72 @@
 use crate::account_updates::AccountUpdate;
 use lencode::prelude::*;
 use solana_hash::Hash;
-use solana_message::v0::LoadedAddresses;
-use solana_transaction::{TransactionResult, versioned::VersionedTransaction};
+use solana_pubkey::Pubkey;
+use solana_signature::Signature;
+use solana_transaction::TransactionResult;
 use solana_transaction_context::TransactionReturnData;
-use solana_transaction_status::{InnerInstructions, Rewards, TransactionTokenBalance};
+use solana_transaction_status::{InnerInstructions, Rewards};
+
+#[derive(Encode, Decode, Debug, Clone, Copy, PartialEq, Eq, Default)]
+#[repr(C)]
+pub struct MessageHeader {
+    pub num_required_signatures: u8,
+    pub num_readonly_signed_accounts: u8,
+    pub num_readonly_unsigned_accounts: u8,
+}
+
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq, Default)]
+#[repr(C)]
+pub struct CompiledInstruction {
+    pub program_id_index: u8,
+    pub accounts: Vec<u8>,
+    pub data: Vec<u8>,
+}
+
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq, Default)]
+#[repr(C)]
+pub struct MessageAddressTableLookup {
+    pub account_key: Pubkey,
+    pub writable_indexes: Vec<u8>,
+    pub readonly_indexes: Vec<u8>,
+}
+
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq, Default)]
+#[repr(C)]
+pub struct LegacyMessage {
+    pub header: MessageHeader,
+    pub account_keys: Vec<Pubkey>,
+    pub recent_blockhash: Hash,
+    pub instructions: Vec<CompiledInstruction>,
+}
+
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq, Default)]
+#[repr(C)]
+pub struct V0Message {
+    pub header: MessageHeader,
+    pub account_keys: Vec<Pubkey>,
+    pub recent_blockhash: Hash,
+    pub instructions: Vec<CompiledInstruction>,
+    pub address_table_lookups: Vec<MessageAddressTableLookup>,
+}
+
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq)]
+#[repr(u8)]
+pub enum VersionedMessage {
+    Legacy(LegacyMessage),
+    V0(V0Message),
+}
+
+impl Default for VersionedMessage {
+    fn default() -> Self {
+        Self::Legacy(LegacyMessage::default())
+    }
+}
 
 /// A Solana transaction with associated metadata and account updates.
 #[derive(Encode, Decode, Debug, Clone)]
 #[repr(C)]
 pub struct Transaction {
-    /// Slot that contains the [`Transaction`]
-    pub slot: u64,
-    /// Index of the transaction within the slot.
-    pub index: usize,
-    /// Transaction signature.
-    pub signature: solana_signature::Signature,
-    /// Hash of the transaction message.
-    pub message_hash: Hash,
-    /// Indicates whether the transaction is a vote.
-    pub is_vote: bool,
     pub status: TransactionResult<()>,
     pub fee: u64,
     pub pre_balances: Vec<u64>,
@@ -29,12 +76,30 @@ pub struct Transaction {
     pub pre_token_balances: Option<Vec<TransactionTokenBalance>>,
     pub post_token_balances: Option<Vec<TransactionTokenBalance>>,
     pub rewards: Option<Rewards>,
-    pub loaded_addresses: LoadedAddresses,
     pub return_data: Option<TransactionReturnData>,
     pub compute_units_consumed: Option<u64>,
     pub cost_units: Option<u64>,
-    /// Fully decoded transaction.
-    pub transaction: VersionedTransaction,
+    /// Signatures for the transaction.
+    pub signatures: Vec<Signature>,
+    /// Message to sign.
+    pub message: VersionedMessage,
     /// Account updates associated with this transaction.
     pub account_updates: Vec<AccountUpdate>,
+}
+
+#[derive(Encode, Decode, Debug, Clone, PartialEq)]
+#[repr(C)]
+pub struct TransactionTokenBalance {
+    pub account_index: u8,
+    pub mint: Pubkey,
+    pub ui_token_amount: TokenAmount,
+    pub owner: Option<Pubkey>,
+    pub program_id: Option<Pubkey>,
+}
+
+#[derive(Encode, Decode, Debug, Clone, Copy, PartialEq, Eq, Default)]
+#[repr(C)]
+pub struct TokenAmount {
+    pub amount: u64,
+    pub decimals: u8,
 }

b4445b9577812c13cf747b1b5a39f0de0a5905b2
Sam Johnson <sam@durosoft.com>
2026-02-01 01:03:07 -0500

    WIP

--- jetstreamer-horizon/src/transactions.rs
+++ jetstreamer-horizon/src/transactions.rs
@@ -1,12 +1,13 @@
+use crate::account_updates::AccountUpdate;
 use lencode::prelude::*;
 use solana_hash::Hash;
-use solana_transaction::versioned::VersionedTransaction;
-use solana_transaction_status::TransactionStatusMeta;
-
-use crate::account_updates::AccountUpdate;
+use solana_message::v0::LoadedAddresses;
+use solana_transaction::{TransactionResult, versioned::VersionedTransaction};
+use solana_transaction_context::TransactionReturnData;
+use solana_transaction_status::{InnerInstructions, Rewards, TransactionTokenBalance};
 
 /// A Solana transaction with associated metadata and account updates.
-#[derive(Default, Encode, Decode, Debug, Clone)]
+#[derive(Encode, Decode, Debug, Clone)]
 #[repr(C)]
 pub struct Transaction {
     /// Slot that contains the [`Transaction`]
@@ -19,8 +20,19 @@ pub struct Transaction {
     pub message_hash: Hash,
     /// Indicates whether the transaction is a vote.
     pub is_vote: bool,
-    /// Status metadata returned by the Solana runtime.
-    pub transaction_status_meta: TransactionStatusMeta,
+    pub status: TransactionResult<()>,
+    pub fee: u64,
+    pub pre_balances: Vec<u64>,
+    pub post_balances: Vec<u64>,
+    pub inner_instructions: Option<Vec<InnerInstructions>>,
+    pub log_messages: Option<Vec<String>>,
+    pub pre_token_balances: Option<Vec<TransactionTokenBalance>>,
+    pub post_token_balances: Option<Vec<TransactionTokenBalance>>,
+    pub rewards: Option<Rewards>,
+    pub loaded_addresses: LoadedAddresses,
+    pub return_data: Option<TransactionReturnData>,
+    pub compute_units_consumed: Option<u64>,
+    pub cost_units: Option<u64>,
     /// Fully decoded transaction.
     pub transaction: VersionedTransaction,
     /// Account updates associated with this transaction.
